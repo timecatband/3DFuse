5286782d11771d99b2cc8ee5538bc2b2d8f773a2
racarr <racarrg@gmail.com>
2023-05-07 01:24:48 -0700

    Add initial generation script

---
+++ utils/generate_initials.py
@@ -0,0 +1,15 @@
+import torch
+import sys
+from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
+
+def load_sd():
+    model_id = "stabilityai/stable-diffusion-2"
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+    pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+    pipe = pipe.to("cuda")
+    return pipe
+
+pipe = load_sd()
+
+image = pipe(sys.argv[1]).images[0]
+image.save(prompt+".png")
