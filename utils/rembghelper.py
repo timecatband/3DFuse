7bf4faa3e709a8c5d742b69dd77f417c8b578f75
racarr <racarrg@gmail.com>
2023-05-07 22:48:41 -0700

    bghelper for dirs

--- utils/rembghelper.py
+++ utils/rembghelper.py
@@ -1,10 +1,22 @@
 from rembg import remove
 import sys
-input_path = sys.argv[1]
-output_path = sys.argv[2]
+input_dir = sys.argv[1]
+output_dir = sys.argv[2]
 
-with open(input_path, 'rb') as i:
-    with open(output_path, 'wb') as o:
-        input = i.read()
-        output = remove(input)
-        o.write(output)
+def rem_bg(input_path, output_path):
+    with open(input_path, 'rb') as i:
+        with open(output_path, 'wb') as o:
+            input = i.read()
+            output = remove(input)
+            o.write(output)
+
+import os
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+# Load each file in input_dir and remove the background
+for filename in os.listdir(input_dir):
+    if filename.endswith(('.jpg', '.png', '.jpeg', '.bmp', '.tiff')):
+        input_path = os.path.join(input_dir, filename)
+        output_path = os.path.join(output_dir, filename)
+        rem_bg(input_path, output_path)
+    
